939d3f6fb5371ab4c2019d559c556fc21e42cda1
pr_Mais <hiimechi@gmail.com>
2021-12-18 17:27:33 +0300

    AuthMode enum & extension

--- lib/views/view_auth.dart
+++ lib/views/view_auth.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+/// The mode of the current auth session, either [AuthMode.login] or [AuthMode.register].
+enum AuthMode { login, register }
+
+extension on AuthMode {
+  String get label => this == AuthMode.login ? 'Login' : 'Register';
+}
+
 class AuthView extends StatefulWidget {
   const AuthView({Key? key}) : super(key: key);
 
@@ -8,6 +15,9 @@ class AuthView extends StatefulWidget {
 }
 
 class _AuthViewState extends State<AuthView> {
+  // We want to start initially from the login mode.
+  AuthMode mode = AuthMode.login;
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
