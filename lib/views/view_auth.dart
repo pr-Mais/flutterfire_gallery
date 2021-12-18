fb2406bdfa31f786755c63d006bf30e59afc31f4
pr_Mais <hiimechi@gmail.com>
2021-12-18 19:38:29 +0300

    fix AuthView layout issues

--- lib/views/view_auth.dart
+++ lib/views/view_auth.dart
@@ -107,115 +107,118 @@ class _AuthViewState extends State<AuthView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30.0),
           child: Form(
             key: formKey,
             autovalidateMode: AutovalidateMode.onUserInteraction,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                  'assets/logo.png',
-                  scale: 1.5,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'FlutterFire \nGallery',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35,
-                    color: Theme.of(context).primaryColor,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/logo.png',
+                    scale: 1.5,
                   ),
-                ),
-                const SizedBox(height: 20),
-                AnimatedError(
-                  text: error,
-                  show: error.isNotEmpty,
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  validator: validator,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'flutter@fire.com',
-                    labelText: 'Email',
-                    prefixIcon: Icon(Icons.email),
+                  const SizedBox(height: 20),
+                  Text(
+                    'FlutterFire \nGallery',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      color: Theme.of(context).primaryColor,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordController,
-                  obscureText: true,
-                  validator: validator,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    prefixIcon: Icon(Icons.lock),
+                  const SizedBox(height: 20),
+                  AnimatedError(
+                    text: error,
+                    show: error.isNotEmpty,
                   ),
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: isLoading ? null : onPasswordAuth,
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: validator,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'flutter@fire.com',
+                      labelText: 'Email',
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    validator: validator,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: isLoading ? null : onPasswordAuth,
+                      child: isLoading
+                          ? const CircularProgressIndicator.adaptive()
+                          : Text(mode.label),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 45,
                     child: isLoading
-                        ? const CircularProgressIndicator.adaptive()
-                        : Text(mode.label),
+                        ? const SizedBox(
+                            width: 45,
+                            child: Center(
+                              child: CircularProgressIndicator.adaptive(),
+                            ),
+                          )
+                        : SignInButton(
+                            Buttons.Google,
+                            elevation: 0.8,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            onPressed: onGoogleAuth,
+                          ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  height: 45,
-                  child: isLoading
-                      ? const SizedBox(
-                          width: 45,
-                          child: Center(
-                            child: CircularProgressIndicator.adaptive(),
-                          ),
-                        )
-                      : SignInButton(
-                          Buttons.Google,
-                          elevation: 0.8,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          onPressed: onGoogleAuth,
+                  const SizedBox(height: 20),
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.bodyText1,
+                      children: [
+                        TextSpan(
+                          text: mode == AuthMode.login
+                              ? "Don't have an account? "
+                              : 'You have an account? ',
                         ),
-                ),
-                const SizedBox(height: 20),
-                RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.bodyText1,
-                    children: [
-                      TextSpan(
-                        text: mode == AuthMode.login
-                            ? "Don't have an account? "
-                            : 'You have an account? ',
-                      ),
-                      TextSpan(
-                        text: mode == AuthMode.login
-                            ? 'Register now'
-                            : 'Click to login',
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            setState(() {
-                              mode = mode == AuthMode.login
-                                  ? AuthMode.register
-                                  : AuthMode.login;
-                            });
-                          },
-                      ),
-                    ],
+                        TextSpan(
+                          text: mode == AuthMode.login
+                              ? 'Register now'
+                              : 'Click to login',
+                          style: const TextStyle(color: Colors.blue),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              setState(() {
+                                mode = mode == AuthMode.login
+                                    ? AuthMode.register
+                                    : AuthMode.login;
+                              });
+                            },
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
